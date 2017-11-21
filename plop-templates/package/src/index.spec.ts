fb077c944cca9e78462cecb9814bed29b91844c6
Mick Dekkers <mickdekkersnl@gmail.com>
2017-11-22 00:24:06 +0100

    chore: remove dummy test body from generated packages

--- plop-templates/package/src/index.spec.ts
+++ plop-templates/package/src/index.spec.ts
@@ -2,6 +2,4 @@ import { AST, Command } from '@vac-dsl/core'
 import {{camelCase (unscope name)}} from './index'
 
 // TODO: write tests for {{camelCase (unscope name)}}
-test('dummy test', () => {
-  expect(true).toBe(true)
-})
+test('dummy test')
