206f6e936a5bdd528e3833fa4ec82314969afa80
Mick Dekkers <mickdekkersnl@gmail.com>
2017-11-27 12:34:19 +0100

    chore: remove old src dir, add example config

--- src/index.js
+++
@@ -1,2 +0,0 @@
-require = require('@std/esm')(module)
-module.exports = require('./src/index.mjs').default
--- src/test.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs')
-const vacDsl = require('./index')
-const input = fs.readFileSync('./program.vac', 'utf8')
-
-const result = vacDsl(input)
-console.log(result)
